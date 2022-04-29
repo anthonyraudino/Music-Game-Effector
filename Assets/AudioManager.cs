4f160f25607d7c505b5c89bdc358cfba475a0b3b
Anth <anthonyraudino@me.com>
2022-04-30 08:55:44 +1000

    added play pause restart button functionality

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -11,6 +11,9 @@ public class AudioManager : MonoBehaviour
     public float mixFx;
 
     public AudioMixer mainMaster;
+    public AudioSource musicSource;
+    public bool isCurrentlyPlaying = false;
+    public bool isCurrentlyPaused = false;
 
     private const string FXVol = "FXVol";
     private const string LoPassVol = "LoPassVol";
@@ -20,35 +23,66 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicSource = GetComponent<AudioSource>();
+        isCurrentlyPlaying = true;
+        isCurrentlyPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void VolumeControl(float vol)
     {
-        vol = Mathf.Log(vol) * 20 ;
+        vol = Mathf.Log(vol) * 20;
         mainMaster.SetFloat(MixerVol, vol);
     }
 
     public void HiPassControl(float vol)
     {
-      //  vol = Mathf.Log(vol) * 20;
+        //  vol = Mathf.Log(vol) * 20;
         mainMaster.SetFloat(HiPassVol, vol);
     }
 
     public void LoPassControl(float vol)
     {
-      //  vol = Mathf.Log(vol) * 20;
+        //  vol = Mathf.Log(vol) * 20;
         mainMaster.SetFloat(LoPassVol, vol);
     }
     public void FXControl(float vol)
     {
         vol = Mathf.Log(vol) * 220;
         mainMaster.SetFloat(FXVol, vol);
+    }
+
+
+    public void PlayMusic()
+    {
+        if (!isCurrentlyPlaying)
+        {
+            musicSource.Play();
+        }
+
+        if (isCurrentlyPaused)
+        {
+            musicSource.Play();
+            isCurrentlyPaused = false;
+        }
+    }
+
+    public void RestartMusic()
+    {
+        musicSource.Stop();
+        musicSource.Play();
+        isCurrentlyPaused = false;
+    }
+
+    public void PauseMusic ()
+    {
+        musicSource.Pause();
+        isCurrentlyPaused = true;
+
     }
 }
